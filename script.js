5f8537f0a3814cf2ee564f3224155f112fbb3c80
uralbekuz1 <uralbektursunov1@gmail.com>
2024-06-23 15:49:19 +0500

    update script.js

--- script.js
+++ script.js
@@ -12,18 +12,3 @@ function myFunction(xml) {
     document.getElementById("misol").innerHTML =
     xmlDoc.getElementsByTagName("distrybut3")[0][1].childNodes[0].nodeValue;
 }
-
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-    myFunction(this);
-    }
-};
-xhttp.open("GET", "https://uralbekuz1.netlify.app/example.xml", true);
-xhttp.send();
-
-function myFunction(xml) {
-    var xmlDoc = xml.responseXML;
-    document.getElementById("misqol").innerHTML =
-    xmlDoc.getElementsByTagName("distrybut4")[0][1].childNodes[0].nodeValue;
-}
