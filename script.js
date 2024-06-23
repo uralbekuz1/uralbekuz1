9a8c20c45ccfff1420ec0e9d2ed0e431782123b3
uralbekuz1 <uralbektursunov1@gmail.com>
2024-06-23 13:26:10 +0500

    update script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    myFunction(this);
+    }
+};
+xhttp.open("GET", "http://uralbekuz1.netlify.app/ex.xml", true);
+xhttp.send();
+
+function myFunction(xml) {
+    var xmlDoc = xml.responseXML;
+    document.getElementById("demo").innerHTML =
+    xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+}
+
+
+// gggg
+document.getElementById("demo1").innerHTML = "Hello World!";
